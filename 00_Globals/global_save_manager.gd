d246f3810ab4c1aafa13ef7484e07b5a624f2b85
yuangenping <550775082@qq.com>
2025-10-05 16:52:39 +0800

    Add pause menu and save/load game functionality
    Introduced a global save manager for saving and loading game state, and added a pause menu with save/load buttons. Updated project settings to register the new systems and input action. Adjusted player HUD and scene transition resources to support these features.

---
+++ 00_Globals/global_save_manager.gd
@@ -0,0 +1,73 @@
+extends Node
+
+
+const SAVE_PATH = "user://"
+
+signal game_loaded
+signal game_saved
+
+var current_save : Dictionary = {
+	scene_path = "",
+	player = {
+		hp = 1,
+		max_hp = 1,
+		pos_x = 0,
+		pos_y = 0
+	},
+	items = [],
+	persistence = [],
+	quests = []
+}
+
+
+func save_game() -> void:
+	update_player_data()
+	update_scene_path()
+	var file := FileAccess.open( SAVE_PATH + "save.sav", FileAccess.WRITE )
+	var save_json = JSON.stringify( current_save )
+	file.store_line( save_json )
+	game_saved.emit()
+	print("save_game", save_json )
+	pass
+	
+func load_game() -> void:
+	var start_time = Time.get_ticks_msec()  # 记录开始时间（毫秒）
+	var file := FileAccess.open( SAVE_PATH + "save.sav", FileAccess.READ )
+	if file == null :
+		return
+	current_save = JSON.parse_string( file.get_line() ) as Dictionary
+	#print("get_save_data",current_save)
+	
+	LevelManager.load_new_level( current_save.scene_path, "", Vector2.ZERO )
+	
+	await LevelManager.level_load_started
+	
+	PlayerManager.hp = current_save.player.hp
+	PlayerManager.max_hp = current_save.player.max_hp
+	PlayerManager.set_player_position( Vector2( current_save.player.pos_x, current_save.player.pos_y ) )
+	PlayerHud.update_hp()
+	
+	await  LevelManager.level_loaded
+	game_loaded.emit()
+	var end_time = Time.get_ticks_msec()   # 记录结束时间
+	var duration = end_time - start_time  # 计算耗时
+	print("函数执行时间：",  duration, "毫秒")  # 打印结果
+	pass
+
+
+func update_player_data() -> void:
+	var p : Player = PlayerManager.player
+	current_save.player.hp = PlayerManager.hp
+	current_save.player.max_hp = PlayerManager.max_hp
+	current_save.player.pos_x = p.global_position.x
+	current_save.player.pos_y = p.global_position.y
+	pass
+
+func update_scene_path() -> void:
+	var p : String = ""
+	for c in get_tree().root.get_children():
+		if c is Level :
+			p = c.scene_file_path
+	
+	current_save.scene_path = p
+	pass
---
+++ GUI/pause_menu/pause_menu.gd
@@ -0,0 +1,56 @@
+extends CanvasLayer
+
+
+@onready var button_save: Button = $VBoxContainer/Button_Save
+@onready var button_load: Button = $VBoxContainer/Button_Load
+
+var is_paused : bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide_pause_menu()
+	button_save.pressed.connect( _on_save_pressed )
+	button_load.pressed.connect( _on_load_pressed )
+	pass # Replace with function body.
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause") :
+		if is_paused == false :
+			show_pause_menu()
+		else :
+			hide_pause_menu()
+		# 防止事件继续传递，给当前按键事件标记为已处理
+		get_viewport().set_input_as_handled()
+	pass
+	
+func show_pause_menu() -> void:
+	get_tree().paused = true
+	visible = true
+	is_paused = true
+	button_save.grab_focus()
+	
+	pass
+	
+func hide_pause_menu() -> void:
+	get_tree().paused = false
+	visible = false
+	is_paused = false
+	pass
+
+
+func _on_save_pressed() -> void:
+	if is_paused == false:
+		return
+		
+	SaveManager.save_game()
+	hide_pause_menu()
+	pass
+	
+func _on_load_pressed() -> void:
+	if is_paused == false:
+		return
+	hide_pause_menu()	
+	SaveManager.load_game()
+	pass
